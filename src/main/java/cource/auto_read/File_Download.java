352deb5415a6b4852fa73055a8e4b70e70f4c5f4
wawade3 <371553474@qq.com>
2020-06-10 20:30:57 +0800

    增加了文件下载

---
+++ src/main/java/cource/auto_read/File_Download.java
@@ -0,0 +1,78 @@
+package cource.auto_read;
+import java.io.*;
+import java.net.URLEncoder;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class File_Download {
+
+    //实现Spring Boot 的文件下载功能，映射网址为/download
+    @RequestMapping("/download")
+    public String downloadFile(HttpServletRequest request,
+                               HttpServletResponse response) throws UnsupportedEncodingException {
+
+        // 获取指定目录下的第一个文件
+        File scFileDir = new File("/home/wawade3/下载/tain_ply");
+        File TrxFiles[] = scFileDir.listFiles();
+        System.out.println(TrxFiles[0]);
+        String fileName = TrxFiles[0].getName(); //下载的文件名
+
+        // 如果文件名不为空，则进行下载
+        if (fileName != null) {
+            //设置文件路径
+            String realPath = "/home/wawade3/下载/tain_ply/";
+            File file = new File(realPath, fileName);
+
+            // 如果文件名存在，则进行下载
+            if (file.exists()) {
+
+                // 配置文件下载
+                response.setHeader("content-type", "application/octet-stream");
+                response.setContentType("application/octet-stream");
+                // 下载文件能正常显示中文
+                response.setHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(fileName, "UTF-8"));
+
+                // 实现文件下载
+                byte[] buffer = new byte[1024];
+                FileInputStream fis = null;
+                BufferedInputStream bis = null;
+                try {
+                    fis = new FileInputStream(file);
+                    bis = new BufferedInputStream(fis);
+                    OutputStream os = response.getOutputStream();
+                    int i = bis.read(buffer);
+                    while (i != -1) {
+                        os.write(buffer, 0, i);
+                        i = bis.read(buffer);
+                    }
+                    System.out.println("Download the song successfully!");
+                }
+                catch (Exception e) {
+                    System.out.println("Download the song failed!");
+                }
+                finally {
+                    if (bis != null) {
+                        try {
+                            bis.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    if (fis != null) {
+                        try {
+                            fis.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+}
