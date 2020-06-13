16179a17305bf5e6e8306277969287df83872283
techluilihua <895130049@qq.com>
2020-06-13 13:17:25 +0800

    增加了学生的成绩和标准答案对比返回学生的分数,优化了置信度输出的结果

--- src/test/java/Algorithm/CompareAnswerTest.java
+++ src/test/java/Algorithm/CompareAnswerTest.java
@@ -7,9 +7,8 @@ class CompareAnswerTest {
 
     @org.junit.jupiter.api.Test
     void compare() {
-        StuAnswer stuAnswer=new StuAnswer(1,"aaa","Google发布的 GFS、MapReduce 和 BigTable 三篇论文对大数据的发展起到重要作用");
-        Answer answer=new Answer(1,"v","1","1");
-        answer.setAnswer("Google发布的三篇论文对大数据的发展起到重要作用");
+        StuAnswer stuAnswer=new StuAnswer(1,"aaa","原豆腐干山豆根发射点风格是则");
+        Answer answer=new Answer(1,"v","原则","1");
         CompareAnswerimpl compareAnswerimpl=new CompareAnswerimpl();
         double score=compareAnswerimpl.compare(stuAnswer,answer);
         System.out.println(score);
